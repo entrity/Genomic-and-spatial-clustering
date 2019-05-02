9abe2229a678af30ffd82eef6de49e3a0eb26a60
Markham <quinesquines@gmail.com>
2019-05-02 07:54:02 -0700

    Start clustering and do scatterplot

--- cluster.py
+++ cluster.py
@@ -7,17 +7,19 @@
 import os, argparse
 import numpy as np
 import scipy.linalg
+import sklearn.cluster
 import util
 from util import tictoc
 
 MAX_EIGVAL = 300
 
 class Clustering(object):
-	def __init__(self, graph, savedir=None):
+	def __init__(self, graph, dim, savedir=None):
 		if isinstance(graph, str):
 			graph = np.load(graph)
 		self.savedir = savedir
 		self.graph   = graph # Adjacency matrix
+		self.dim     = dim   # Dimensionality, i.e. number of eigenvectors to use in embedding. If 'None', then all will be used
 		self.Lu      = None  # Unnormalized Laplacian
 		self.Ln      = None  # Normalized Laplacian
 		self.Lrw     = None  # Random-walk Laplacian
@@ -48,15 +50,30 @@ class Clustering(object):
 			self.Lrw = np.matmul(rwD, self.unnormalized_laplacian())
 			tictoc('Made Random-walk Laplacian.', tic)
 		return self.Lrw
-	# Return embedding based on Normalized Laplacian
-	def embedding_n(self):
-		self._embedding( self.normalized_laplacian(), 'normalized' )
-	# Return embedding based on Random-walk Laplacian
-	def embedding_rw(self):
-		self._embedding( self.random_walk_laplacian(), 'random-walk' )
-	# def _embedding(self, laplacian, name):
-	# 	if self.savedir is not None:
-
+	def cluster_n(self, k):
+		embedding = self._embedding( self.normalized_laplacian(), 'normalized' )
+		return self._cluster(embedding, k)
+	def cluster_rw(self, k):
+		embedding = self._embedding( self.random_walk_laplacian(), 'random-walk' )
+		return self._cluster(embedding, k)
+	def _embedding(self, laplacian, name):
+		vals, vecs = eigen(laplacian)
+		if self.dim is not None:
+			I = vals.argsort()
+			vecs = vecs[I, :self.dim]
+		return vecs
+	def _cluster(self, embedding, k):
+		tic = util.tictoc('clustering...')
+		kmeans = sklearn.cluster.KMeans(k).fit(embedding)
+		util.tictoc('clustered.', tic)
+		self._save('kmeans-dim-%s-k-%d' % (str(self.dim), k), kmeans)
+		return kmeans
+	def _save(self, name, obj):
+		if self.savedir is not None:
+			path = os.path.join(self.savedir, name)
+			util.debug('Saving %s...' % path)
+			with open(path, 'wb') as fout:
+				pickle.dump(obj, path)
 
 # Return eigendecomposition: vals, vecs
 def eigen(laplacian):
@@ -74,25 +91,47 @@ def plot_eigvals(graph, k, title, savedir):
 		x = np.arange(0, len(vals))
 		plt.plot(x, vals)
 		fig.savefig(os.path.join(savedir, title.replace(' ','-')))
-	clustering = Clustering(graph, k)
+	clustering = Clustering(graph, None)
 	lN  = clustering.normalized_laplacian()
 	_plot_eigvals(lN, title('Normalized'))
 	lRw = clustering.random_walk_laplacian()
 	_plot_eigvals(lN, title('Random-walk'))
 
-def run(graph, k):
-	clustering = Clustering(graph)
+
+def plot_clusters(xy_npy, kmeans):
+	xy = np.load(xy_npy)
+	print(len(kmeans.labels_))
+	print(np.histogram(kmeans.labels_, bins=np.arange(0,13)-0.5))
+	import matplotlib.pyplot as plt
+	clr = kmeans.labels_
+	# clr = np.arange(0, xy.shape[0]) % 3
+	# print(clr)
+	# import IPython; IPython.embed(); # to do: delete
+	plt.scatter(xy[:,0], xy[:,1], c=clr)
+	plt.show()
+	# import IPython; IPython.embed(); # to do: delete
+
+
+def run(graph, dim, k, do_plot, xy_npy):
+	print(dim, k)
+	clustering = Clustering(graph, dim)
+	kmeans = clustering.cluster_n(k)
+	if do_plot: plot_clusters(xy_npy, kmeans)
+	# clustering.cluster_rw(k)
+	# if do_plot: plot_clusters(xy_npy, kmeans)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	util.add_default_args(parser)
 	parser.add_argument('--knn', type=int, help='Number of nearest neighbours to preserve in sparse graph')
 	parser.add_argument('--km', type=int, default=12, help='Number of nearest clusters for k-means clustering. The default of 12 was chosen because that\'s how many class labels Gerald indicated.')
+	parser.add_argument('--dim', type=int, help='Number of dimensions (eigenvectors) to use in embedding.')
 	parser.add_argument('-g', '--graph', help='Path to adjacency matrix npy file.')
+	parser.add_argument('--scatterplot', action='store_true', help='Make a 2D scatterplot of the cells.')
 	parser.add_argument('--eigplot', action='store_true', help='Only plot eigenvalues. Don\'t run clustering.')
 	args = parser.parse_args()
 	if args.eigplot:
 		title = lambda lap_name: 'Eigenvalues %s (connectivity %d)' % (lap_name, args.knn)
 		plot_eigvals(args.graph, args.km, title, args.savedir)
 	else:
-		run(args.graph, args.km)
+		run(args.graph, args.dim, args.km, args.scatterplot, args.xy_npy)
