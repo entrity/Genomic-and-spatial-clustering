83f58d32760fc735ec5c7ba62ad180a254f8854e
Markham <quinesquines@gmail.com>
2019-05-02 07:54:22 -0700

    Add experiments script

--- experiments.sh
+++ experiments.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-for K in 4 8 16 32; do
-	OUT=intermediates/knn-$K
+function run () {
 	mkdir -p "$OUT"
-	./graphs.py -k $K -s "$OUT"
+	./graphs.py --knn $K --savedir "$OUT" "${@}"
+	./cluster.py --knn $K --km $KM --scatterplot --savedir "$OUT" --graph "$OUT/sparse-graph.npy"
+}
 
+KM=12
+for K in 4 8 16 32; do
+	OUT=intermediates/no-spatial-knn-$K
+	run --no-spatial
+	OUT=intermediates/knn-$K
+	run
+	break
 done
